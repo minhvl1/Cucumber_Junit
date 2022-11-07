ce87bbe579b0946449bcbf6e19bdbf66d258c7a9
nhuthai.n <nhuthai.n@trustana.com>
2022-11-07 09:53:32 +0700

    run cucumber cmd

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,18 +1,21 @@
 package runner;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-import static io.cucumber.testng.CucumberOptions.SnippetType.CAMELCASE;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty","html:target/cucumber","summary"}
-        ,features = {"src/test/resources"}
-        ,glue = {"Steps"}
+        plugin = {"pretty","junit:target/JUnitReports/report.xml",
+        "json:target/JSONReports/report.json",
+        "html:target/HtmlReports"},
+        monochrome = true,
+        features = {"src/test/resources/Google.feature"},
+        glue = {"Steps"}
 //        ,tags= "@scenario1"
-        ,snippets = CAMELCASE
+
 )
 
 public class TestRunner {
