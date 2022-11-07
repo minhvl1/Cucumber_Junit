982b9ec340ed31731c187d5e2e8268330a9baeea
nhuthai.n <nhuthai.n@trustana.com>
2022-11-07 15:18:27 +0700

    extent report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -12,7 +12,8 @@ import org.junit.runner.RunWith;
                 "junit:target/JUnitReports/report.xml",
 //                "json:target/JSONReports/report.json",
                 "json:target/cucumber.json",
-                "html:target/HtmlReports"},
+                "html:target/HtmlReports",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         features = {"src/test/resources/"},
         glue = {"Steps"}
 
